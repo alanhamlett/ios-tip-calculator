dd4553e2d0ee0ddb0eb912da63dc7677e639d51d
Alan Hamlett <alan.hamlett@airbnb.com>
2017-03-15 15:15:25 -0700

    add settings view controller

---
+++ TipCalculator/SettingsViewController.h
@@ -0,0 +1,13 @@
+//
+//  SettingsViewController.h
+//  TipCalculator
+//
+//  Created by alan_hamlett on 3/15/17.
+//  Copyright © 2017 Alan Hamlett. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SettingsViewController : UIViewController
+
+@end
